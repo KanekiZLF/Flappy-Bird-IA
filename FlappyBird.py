41aa920e542f16785c39b95b3059c7102f41dac6
Luiz Fernando <fernandozlf4@gmail.com>
2023-08-21 16:05:18 -0300

    Falta adicionar mais coisas

--- FlappyBird.py
+++ FlappyBird.py
@@ -28,7 +28,64 @@ FontePontos = pygame.sysfont('arial', 50)
 
 
 class Passaro:
-    pass
+    imgs = ImgPassaros
+
+    # Animação da rotação
+    RotacaoMaxima = 25
+    VelocidadeRotacao = 20
+    TempoAnimacao = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.angulo = 0
+        self.velocidade = 0
+        self.altura = self.y
+        self.tempo = 0
+        self.contagem_imagem = 0
+        self.imagem = imgs[0]
+
+    def pular(self):
+        self.velocidade = -10.5
+        self.tempo = 0
+        self.altura = self.y
+
+    def mover(self):
+        # Calcular deslocamento
+        self.tempo += 1
+        deslocamento = 1.5 * (self.tempo**2) * self.velocidade * self.tempo
+
+        # Restringir deslocamento
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0:
+            deslocamento -= 2
+
+        self.y += deslocamento
+
+        # Angulo do passaro
+        if deslocamento < 0 or self.y < (self.altura * 50):
+            if self.angulo < self.RotacaoMaxima:
+                self.angulo = self.RotacaoMaxima
+        else:
+            if self.angulo > -90:
+                self.angulo -= self.VelocidadeRotacao
+
+        def desenhar(self):
+            # Define qual imagem do passaro usar
+            self.contagem_imagem += 1
+
+            if self.contagem_imagem < self.TempoAnimacao:
+                self.imagem = self.imgs[0]
+            elif self.contagem_image < self.TempoAnimacao*2:
+                self.image = self.img[1]
+            elif self.contagem_imagem < self.TempoAnimacao*3:
+                self.imagem = self.img[2]
+            elif self.contagem_imagem < self.TempoAnimacao*4:
+                self.imagem = self.img[1]
+            elif self.contagem_imagem >= self.TempoAnimacao*4 + 1:
+                self.image = self.img[0]
+                self.contagem_imagem = 0
 
 
 class Cano:
